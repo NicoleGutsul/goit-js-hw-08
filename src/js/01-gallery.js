5832c7f1dfa54c003205f1816ee8ffd966767074
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-02-04 20:07:50 +0200

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const originalGalleryEl = galleryItems.map((image) => {
+   return `<a class="gallery__item" href="${image.original}">
+     <img
+       class="gallery__image"
+       src="${image.preview}"
+       alt="${image.description}"
+     />
+   </a>
+   `
+}).join("");
+
+galleryEl.insertAdjacentHTML('afterbegin', originalGalleryEl);
+
+new SimpleLightbox('.gallery a', {
+   captionsData: 'alt',
+   captionDelay: 250,
+ });
