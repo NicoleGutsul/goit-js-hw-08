168d6bb742fa3b36fe7dc892f0abb111cfca7164
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-02-04 20:59:49 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+const throttle = require('lodash.throttle');
 
+const INPUT_KEY = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+const formData = {};
+populateTextarea();
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(INPUT_KEY);
+};
+
+function onTextareaInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(INPUT_KEY, JSON.stringify(formData));
+};
+
+function populateTextarea() {
+    let keepMessage = localStorage.getItem(INPUT_KEY);
+    if (keepMessage) {
+        keepMessage = JSON.parse(keepMessage);
+        Object.entries(keepMessage).forEach(([name, value]) => {
+            refs.form.elements[name].value = value;
+        });
+
+    }
+}
